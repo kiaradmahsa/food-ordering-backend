26061efeb0e24bcda41203afebf30a161b0c4168
mahsa <kiaradmahsa@gmail.com>
2024-03-09 16:00:23 +0000

    Validate Access Token

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -1,8 +1,11 @@
 import express  from "express";
 import myUserController from "../controllers/myUserController";
+import { jwtCheck } from "../middleware/auth";
+
+
 const router = express.Router();
 
 // /api/my/user
-router.post("/", myUserController.createCurrentUser);
+router.post("/",jwtCheck, myUserController.createCurrentUser);
 
 export default router;
